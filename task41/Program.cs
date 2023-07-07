da4ca77579fc465e461ff065c54f2d35c6fc7868
Oleg Yatmanov <o_yatmanov@mail.ru>
2023-07-07 17:54:35 +0500

    Семинар 10 домашнее задание

---
+++ task41/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+Write($"Введи число М(количество чисел): ");
+int m = int.Parse(ReadLine());
+int[] massiveNumbers = new int[m];
+
+void InputNumbers(int m){
+for (int i = 0; i < m; i++)
+  {
+    Write($"Введи {i+1} число: ");
+    massiveNumbers[i] = int.Parse(ReadLine());
+  }
+}
+int Comparison(int[] massiveNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < massiveNumbers.Length; i++)
+  {
+    if(massiveNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+InputNumbers(m);
+WriteLine($"Введено чисел больше 0: {Comparison(massiveNumbers)} ");
---
+++ task43/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using static System.Console;
+Clear();
+
+double InputNum (string message)
+{
+    Write (message);
+    return int.Parse(ReadLine ());
+}
+double b1 = InputNum ($"Задайте b1: ");
+double k1 = InputNum ($"Задайте k1: ");
+double b2 = InputNum ($"Задайте b2: ");
+double k2 = InputNum ($"Задайте k2: ");
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * (b2 - b1) / (k1 - k2) + b1;
+
+WriteLine ($"Точка пересечения прямых: ({x}, {y})");
